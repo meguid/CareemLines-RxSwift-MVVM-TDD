2f25467edc5502ef96877c91bb21bb5b56abdc09
Ahmed Meguid <ameguid12@gmail.com>
2018-08-12 17:13:56 +0200

    adding the routes item

---
+++ CareemLines/Recent Routes/RecentRoutes.swift
@@ -0,0 +1,16 @@
+//
+//  RecentRoutes.swift
+//  CareemLines
+//
+//  Created by Ahmed Abdel Meguid on 8/12/18.
+//  Copyright © 2018 Ahmed Abdel Meguid. All rights reserved.
+//
+
+import Foundation
+
+struct RecentRoutes {
+    let date: Date
+    let startStop: String
+    let endStop: String
+    let price: Int
+}
